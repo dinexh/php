f66dd456240843159e7c7c678a35f9739f38ca3d
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-06-23 22:39:55 +0530

    if else and else if conditional statements are added

---
+++ Learn/condition.php
@@ -0,0 +1,13 @@
+<?php
+    // $age = 0;
+    // if($age >= 18){
+    //     echo "You are eligible to vote";
+    // }
+    // elseif($age == 0){
+    //     echo "You are just born";
+    // }
+    // else{
+    //     echo "You are not eligible to vote";
+    // }
+    $adult = true;
+?>
