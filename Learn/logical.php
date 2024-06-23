c4cd81f6389ba7bf3cc2d4221d587ce8b97e2b29
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-06-23 22:44:20 +0530

    added logical operators

---
+++ Learn/logical.php
@@ -0,0 +1,12 @@
+<?php
+    $temp = -30;
+    // && and operator
+    // || or operator
+    if($temp >= 0 || $temp<= 30){
+        echo "its fine";
+    }
+    else
+    {
+        echo "its not fine";
+    }
+?>
